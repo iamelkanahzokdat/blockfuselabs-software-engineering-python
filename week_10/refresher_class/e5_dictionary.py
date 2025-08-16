9a8d65e734c4fab31dcd0b5054132d356bf2d7a2
iamelkanahzokdat <nanmaelkanah388@gmail.com>
2025-08-16 23:35:09 +0100

    update files

--- week_10/refresher_class/e5_dictionary.py
+++ week_10/refresher_class/e5_dictionary.py
@@ -1,16 +1,72 @@
 contacts = {
-       "Dre": 70000,
-       "Mimi": 8000,
-       "TK": 4300,
-       "Julius": 5000,
-       "Ajor": 4000,
-       "Bankat": 4500,
-       "Dimka": 4000,
-       "Bamshak": 6000,
-       "Jaz": 4000,
-       "Hillary": 6500
+       "Dre": "09138890925",
+       "Mimi": "08032568018",
+       "TK": "043004988494",
+       "Julius": "07094950400",
+       "Ajor": "09038340300",
+       "Bankat": "09048594500",
+       "Dimka": "04056785400",
+       "Bamshak": "07090960089",
+       "Jaz": "0704066705678",
+       "Hillary": "080609606500",
 }
 
-
-
+print(len(contacts), type(contacts))
+print(contacts["Mimi"])
 print(contacts)
+
+
+
+
+
+trial = {
+     True: "True",
+     False: "False",
+     4: "One",
+     4.0: "One Zero",
+     (1,2): "One comma Zero"
+}
+
+print(trial)
+print(trial[False])
+
+
+
+trial = {"scores": (1,2)}
+one, two = trial["scores"]
+print(one, two)
+
+
+
+#contacts.update
+#contacts.popitem
+#contacts.pop
+
+
+laptop = {
+       "Dorcas": ["a", "z", "c", "d"],
+       "Dan": ["a", "b"],
+       "Ben": ["X", 8, "r"],
+}
+
+laptop["Dorcas"] += "b"
+laptop["Dan"] += "Z"
+laptop["Ben"] += "D"
+
+print(laptop)
+
+
+cart = {}
+
+Dan = cart
+Dre = cart
+Jibrin = cart
+print(id(cart))
+
+Dan.update({"milkshake": 1})  
+Dre.update({"coca cola": 2})
+Jibrin.update({"red bulls": 2}) 
+
+print(cart)
+
+cart.clear()
---
+++ week_10/refresher_class/e6_conditionals.py
@@ -0,0 +1,43 @@
+# True or False Statements
+
+
+students = ["TK", "Ta", "Bethel", "Dre", "Dan", "Ben"]
+print("TK" in students)
+
+
+contacts = {
+          "Abel": "0703",
+         "Aaron": "0803",
+          "Ben": "0813"
+           }
+
+print("Abel" in contacts)
+print("jibrin" not in contacts)
+print("jibrin" in contacts)
+
+# !----and----or
+print(False or True and False or True or not False and True)
+
+
+favourite_number = 10
+
+if favourite_number == 10:
+   print(favourite_number)
+
+
+
+
+temp = 28
+result = " "
+
+if temp < 15 and temp < 15:
+   result ="The weather is cold"
+
+elif temp >= 25 and temp <= 25:
+   result = "The weather is warm"
+
+else:
+   result = "The weather is Hot"
+
+print(result)
+
